de4cffeb6c22a6e99cecd8f1e0b273cccc827d92
vinaycode98 <guremattiv888@gmail.com>
2024-07-05 13:07:44 +0530

    new

---
+++ src/main/java/com/vinayak/docker/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.vinayak.docker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	@RequestMapping("/")
+	public String hello() {
+		return "hello world";
+	}
+
+}
